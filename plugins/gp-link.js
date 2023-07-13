c476e69618cdcfc761b20ef55fe2277d6ccc7fd6
kinflux digital <126244085+diggilly@users.noreply.github.com>
2023-07-13 18:27:46 +0300

    Update gp-link.js

--- plugins/gp-link.js
+++ plugins/gp-link.js
@@ -2,12 +2,12 @@ import { areJidsSameUser } from '@adiwajshing/baileys'
 let handler = async (m, { conn, args }) => {
     let group = m.chat
     if (/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(args[0])) group = args[0]
-    if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw '⚠️ Can only be used in groups'
+    if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw '⚠️ hello kinflux bot user Can only be used in groups'
     let groupMetadata = await conn.groupMetadata(group)
     if (!groupMetadata) throw 'groupMetadata is undefined :\\'
-    if (!('participants' in groupMetadata)) throw 'participants is not defined :('
+    if (!('participants' in groupMetadata)) throw 'hello kinflux bot user participants is not defined :('
     let me = groupMetadata.participants.find(user => areJidsSameUser(user.id, conn.user.id))
-    if (!me) throw '✳️ I am not in that group :('
+    if (!me) throw '✳️ hello kinflux bot user I am not in that group :('
     if (!me.admin) throw '✳️ I am not an administrator'
     m.reply('https://chat.whatsapp.com/' + await conn.groupInviteCode(group))
 }
